7400c24eefbd5dc4601fbdccf27674fb6dab380f
Султан Алексеевич Хожаниязов <998909505181@ya.ru>
2024-07-18 23:54:44 +0500

    add image in activites + adaptive

--- src/components/Activity/ActivityItem.vue
+++ src/components/Activity/ActivityItem.vue
@@ -6,6 +6,12 @@ const props = defineProps({
     activity: Object
 })
 
+const nftsPath = {
+    'bm': 'bi-mech',
+    'qp': 'quartan_primes',
+    'ap': 'alpha_prestige',
+}
+
 const route = useRoute()
 
 const date = new Date(props.activity.timestamp * 1000);
@@ -14,9 +20,9 @@ const date = new Date(props.activity.timestamp * 1000);
 <template>
     <td class="flex justify-center"><img :src="platformIcon[activity.platform]" alt="" class="w-6 h-6"></td>
     <td>{{ activity.price_usd }} usdt</td>
-    <td>{{ pageNames[route.name].name }} #{{ activity.tokenId }}</td>
-    <td class="hover:opacity-50 transition-all"><a :href="'https://element.market/account/' + activity.from_address">{{ activity.from_address.slice(0, 6) + '...' + activity.from_address.slice(-3) }}</a></td>
-    <td class="hover:opacity-50 transition-all"><a :href="'https://element.market/account/' + activity.to">{{ activity.to.slice(0, 6) + '...' + activity.to.slice(-3) }}</a></td>
-    <td class="hover:opacity-50 transition-all"><a :href="(activity.chain === 'BSC' ? 'https://bscscan.com/tx/' : 'https://etherscan.io/tx/') + activity.txHash">{{ activity.txHash.slice(0, 6) + '...' + activity.txHash.slice(-3) }}</a></td>
+    <td><img :src='"/nft/" + nftsPath[route.name] + "/" + pageNames[route.name].name + "-" + activity.tokenId + ".jpg"' class='w-10 float-left mr-2'>{{ pageNames[route.name].name }} #{{ activity.tokenId }}</td>
+    <td class="hover:opacity-50 transition-all hidden sm:table-cell"><a :href="'https://element.market/account/' + activity.from_address">{{ activity.from_address.slice(0, 6) + '...' + activity.from_address.slice(-3) }}</a></td>
+    <td class="hover:opacity-50 transition-all hidden sm:table-cell"><a :href="'https://element.market/account/' + activity.to">{{ activity.to.slice(0, 6) + '...' + activity.to.slice(-3) }}</a></td>
+    <td class="hover:opacity-50 transition-all hidden sm:table-cell"><a :href="(activity.chain === 'BSC' ? 'https://bscscan.com/tx/' : 'https://etherscan.io/tx/') + activity.txHash">{{ activity.txHash.slice(0, 6) + '...' + activity.txHash.slice(-3) }}</a></td>
     <td class="text-right">{{ date.toLocaleString() }}</td>
 </template>
