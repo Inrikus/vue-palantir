4fdfddfcd915dbafeab705c891c2428383593251
Султан Алексеевич Хожаниязов <998909505181@ya.ru>
2024-07-22 01:06:01 +0500

    Added Inrikusa HOTELKI

--- src/components/Banner/BannerItem.vue
+++ src/components/Banner/BannerItem.vue
@@ -8,8 +8,8 @@ defineProps({
 </script>
 
 <template>
-<div class='relative'>
-    <div class='bg-[#1F1F1F] w-full shadow-2xl min-h-full rounded-xl py-20 justify-between px-16 flex flex-col-reverse xl:flex-row items-center'>
+<div class='relative hover:translate-y-[-5px] transition-all'>
+    <router-link :to='bannerLinks[bannerName].link_to' class='bg-[#1F1F1F] w-full shadow-2xl hover:shadow-[0 50px 75px -12px rgb(0 0 0 / 0.25);] min-h-full rounded-xl py-20 justify-between px-16 flex flex-col-reverse xl:flex-row items-center'>
         <div class="flex flex-col gap-6 items-center xl:items-start w-full justify-center">
             <div class='flex flex-col gap-2 text-xl lg:text-2xl font-semibold text-[#63b4c8] items-center xl:items-start'>
                 <h3 class='text-4xl lg:text-6xl xl:text-7xl font-semibold'>{{ bannerLinks[bannerName].name }}</h3>
@@ -25,12 +25,12 @@ defineProps({
                 </div>
             </div>
 
-            <router-link :to='bannerLinks[bannerName].link_to' class="border-2 border-[#63b4c8] text-[#63b4c8] rounded-md p-2 flex gap-2 text-4xl font-semibold items-center hover:bg-gray-600 transition-all">Open</router-link>
+            
         </div>
 
         <div class="px-auto max-w-[320px]">
             <img :src='bannerLinks[bannerName].image' class="rounded-2xl h-[240px] w-[320px] object-cover">
         </div>
-    </div>
+    </router-link>
 </div>
 </template>
--- src/components/Cards/CardItem.vue
+++ src/components/Cards/CardItem.vue
@@ -14,11 +14,11 @@ defineProps({
         </div>
 
         <div class="ml-2 self-start mt-2">
-            <p class="text-sm font-bold tracking-[1px]">{{ card.nft_name }}</p>
+            <p class="text-lg font-bold tracking-[1px]">{{ card.nft_name }}</p>
             <p class="text-sm font-bold tracking-[1px] mt-2">
                 <span class="inline-flex flex-col">
-                    <span><img :src="'/currency/' + (Object.keys(currency).includes(card.price_native.currency) ? currency[card.price_native.currency] : 'unknown.svg')" class="w-4 h-4 float-left mr-2">{{ card.price_native.value }}</span>
-                    <span class="text-sm opacity-40">{{ card.price ? card.price + ' USD' : "Not Listed" }}</span>
+                    <span class="flex items-center text-lg"><img :src="'/currency/' + (Object.keys(currency).includes(card.price_native.currency) ? currency[card.price_native.currency] : 'unknown.svg')" class="w-4 h-4 float-left mr-2">{{ card.price_native.value }}</span>
+                    <span class="text-sm opacity-75">{{ card.price ? card.price + ' USD' : "Not Listed" }}</span>
                 </span>
             </p>
         </div>
