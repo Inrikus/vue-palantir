2d6bcfefb418f0c0627c0ad0477150253c970dee
Inrikus <mbyxarov92@mail.ru>
2024-07-22 05:32:11 +0500

    Alberto Suleverin Update

--- src/components/Activity/ActivityItem.vue
+++ src/components/Activity/ActivityItem.vue
@@ -14,11 +14,11 @@ const nftsPath = {
 }
 
 var options = {
-  year: 'numeric',
-  month: 'numeric',
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
 };
 
 const route = useRoute()
@@ -27,19 +27,42 @@ const date = new Date(props.activity.timestamp * 1000);
 </script>
 
 <template>
-    <span class="h-full"><div class="w-full h-full flex items-center justify-center"><img :src="platformIcon[activity.platform]" alt="" class="w-6 h-6"></div></span>
-    <span class="flex items-center"><img :src='"/nft/" + nftsPath[route.name] + "/" + (route.name !== "pf" ? pageNames[route.name].name + "-" + activity.tokenId + ".jpg" : activity.tokenId > 10000 ? "Steam-Key-NFT.png" : "Beta-Testing-NFT.png")' class='w-10 float-left mr-2'>{{ pageNames[route.name].name }} #{{ activity.tokenId }}</span>
+    <span >
+        <div class="w-full h-full flex items-center">
+            <img :src="platformIcon[activity.platform]" alt="" class="w-7 h-7">
+        </div>
+    </span>
+    <span class="flex items-center">
+        <img :src='"/nft/" + nftsPath[route.name] + "/" + (route.name !== "pf" ? pageNames[route.name].name + "-" + activity.tokenId + ".jpg" : activity.tokenId > 10000 ? "Steam-Key-NFT.png" : "Beta-Testing-NFT.png")'
+            class='w-10 float-left mr-2'>
+        {{ pageNames[route.name].name }} #{{ activity.tokenId }}
+    </span>
     <span class="align-middle">
         <div class="w-full flex items-end flex-col">
-            <p><img :src="'/currency/' + (activity.currency in currency ? currency[activity.currency] : 'unknown.svg')" class="w-4 h-4 float-left mr-2 mt-1">{{ activity.price_native }}</p>
+            <p class="flex items-center">
+                <img :src="'/currency/' + (activity.currency in currency ? currency[activity.currency] : 'unknown.svg')"
+                    class="w-4 h-4 mr-2">
+                {{ activity.price_native }}
+            </p>
             <p>{{ activity.price_usd }}$</p>
         </div>
     </span>
-    <span class="hover:opacity-50 transition-all hidden md:inline text-center"><a :href="'https://element.market/account/' + activity.from_address">{{ activity.from_address.slice(0, 6) + '...' + activity.from_address.slice(-3) }}</a></span>
-    <span class="hover:opacity-50 transition-all hidden md:inline text-center"><a :href="'https://element.market/account/' + activity.to">{{ activity.to.slice(0, 6) + '...' + activity.to.slice(-3) }}</a></span>
-    <span class="hover:opacity-50 transition-all hidden md:table-cell"><a :href="(activity.chain === 'BSC' ? 'https://bscscan.com/tx/' : 'https://etherscan.io/tx/') + activity.txHash">{{ activity.txHash.slice(0, 6) + '...' + activity.txHash.slice(-3) }}</a></span>
-    <span class="text-right">{{ date.toLocaleString('US-us', options) }}</span>
+    <span class="hover:opacity-50 transition-all hidden md:inline text-center">
+        <a :href="'https://element.market/account/' + activity.from_address">
+            {{ activity.from_address.slice(0, 6) + '...' + activity.from_address.slice(-3) }}
+        </a>
+    </span>
+    <span class="hover:opacity-50 transition-all hidden md:inline text-center">
+        <a :href="'https://element.market/account/' + activity.to">
+            {{ activity.to.slice(0, 6) + '...' + activity.to.slice(-3) }}
+        </a>
+    </span>
+    <span class="hover:opacity-50 transition-all text-center">
+        <a
+            :href="(activity.chain === 'BSC' ? 'https://bscscan.com/tx/' : 'https://etherscan.io/tx/') + activity.txHash">
+            {{ date.toLocaleString('en-US', options).replace(',', '') }}
+        </a>
+    </span>
 </template>
 
-<style>
-</style>
+<style></style>
--- src/components/Activity/ActivityPanel.vue
+++ src/components/Activity/ActivityPanel.vue
@@ -31,15 +31,15 @@ onBeforeUnmount(() => {
     <div class="grid-table w-full mx-auto text-[#63B4C8] text-sm sm:text-l xl:text-xl font-normal sm:font-semibold">
         <div class="grid-table-line items-center border-b-2 border-[#63B4C8] pb-2 border-opacity-50">
             <span class="text-start">Market</span>
-            <span>NFT</span>
+            <span class="text-start">NFT</span>
             <span class="text-end">Price</span>
             <span class="hidden md:inline text-center">From</span>
             <span class="hidden md:inline text-center">To</span>
-            <span class="hidden md:inline">txHash</span>
-            <span class="text-right">Date</span>
+            <span class="text-center">Date</span>
         </div>
 
-        <div v-for="activity in activityData" :key="activity.id" class="grid-table-line items-center hover:bg-gray-800 hover:border-2 hover:border-[#63B4C8] p-2 rounded-xl">
+        <div v-for="activity in activityData" :key="activity.id"
+            class="grid-table-line items-center hover:bg-gray-800 hover:border-2 hover:border-[#63B4C8] p-2 rounded-xl">
             <ActivityItem :activity="activity" />
         </div>
 
@@ -47,21 +47,32 @@ onBeforeUnmount(() => {
 </template>
 
 <style>
-    .grid-table {
-        display: flex;
-        flex-direction: column;
-        gap: 20px;
-    }
+.grid-table {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
 
+.grid-table-line {
+    display: grid;
+    grid-template-columns: 0.5fr 1.5fr 1fr 1fr;
+    gap: 10px;
+}
+
+@media (min-width: 768px) {
     .grid-table-line {
-        display: grid;
-        grid-template-columns: 0.5fr 2fr 1fr 1fr;
-        gap: 10px;
+        grid-template-columns: 0.5fr 1.5fr 1fr 2fr 2fr 1.5fr;
     }
+}
 
-    @media (min-width: 768px) {
-        .grid-table-line {
-            grid-template-columns: 0.25fr 2fr 0.75fr 2fr 2fr 2fr 1fr;
-        }
+@media (max-width: 767px) {
+    .grid-table-line {
+        grid-template-columns: 0.5fr 1.5fr 1fr 1fr;
     }
+}
+
+.grid-table-line span {
+    min-width: 0;
+    /* Позволяет элементам уменьшаться до минимального размера */
+}
 </style>
