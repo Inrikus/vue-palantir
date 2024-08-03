be7cfcf6185e74fb59717ab5239b15832f2aa184
Inrikus <mbyxarov92@mail.ru>
2024-08-04 03:00:36 +0500

    Google Analytics

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
-
+import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -13,5 +13,10 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(autoAnimatePlugin)
+
+// Настройка vue-gtag
+app.use(VueGtag, {
+    config: { id: 'G-5NRKG76JQG' } // Замените GA_MEASUREMENT_ID на ваш идентификатор Google Analytics
+},router)
 
 app.mount('#app')
