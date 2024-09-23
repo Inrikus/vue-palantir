8898bb6cf8d043297b34dcd6f4ceb21b7d09ea8d
Inrikus <mbyxarov@gmail.com>
2024-09-23 12:33:29 +0300

    Planet Collection

--- src/utils/dictsList.js
+++ src/utils/dictsList.js
@@ -1,27 +1,39 @@
 export const filterList = {
-    bm: {
-      GRADE: ['Common', 'Elite', 'Epic', 'Legend', 'Mythical'],
-      Class: ['Buster', 'Keystone', 'Striker'],
-      Weapons: ['1', '2', '3', '4']
-    },
-    qp: {
-      GRADE: ['Elite', 'Epic', 'Legend', 'Mythical'],
-      Class: ['Buster', 'Keystone', 'Striker'],
-      Weapons: ['2', '3', '4']
-    },
-    peace: {
-      Rarity: ['Common', 'Rare', 'Elite', 'Epic', 'Legendary', "Collector's"]
-    },
-    ap: {},
-    pf: {}
+  bm: {
+    GRADE: ['Common', 'Elite', 'Epic', 'Legend', 'Mythical'],
+    Class: ['Buster', 'Keystone', 'Striker'],
+    Weapons: ['1', '2', '3', '4']
+  },
+  qp: {
+    GRADE: ['Elite', 'Epic', 'Legend', 'Mythical'],
+    Class: ['Buster', 'Keystone', 'Striker'],
+    Weapons: ['2', '3', '4']
+  },
+  peace: {
+    Rarity: ['Common', 'Rare', 'Elite', 'Epic', 'Legendary', "Collector's"]
+  },
+  ap: {},
+  pf: {
+    Grade: ['Tier 1', 'Tier 2', 'Tier 3', 'Tier 4', 'Tier 5'],
+    Ecology: ['Grassland', 'Land', 'Ocean', 'Lava', 'Wild', 'Gas', 'Poison', 'Frozen', 'Desert'],
+    'Satellite Amount': ['0', '2', '4', '6', '8'],
+    'Mineral Amount': ['2', '4', '7', '11', '16'],
+    Titanium: ['0', '1', '2', '3', '4', '5'],
+    Coal: ['0', '1', '2', '3', '4', '5'],
+    Sulfur: ['0', '1', '2', '3', '4', '5'],
+    Petroleum: ['0', '1', '2', '3', '4', '5'],
+    Helium: ['0', '1', '2', '3', '4', '5'],
+    Hydrogen: ['0', '1', '2', '3', '4', '5'],
+  }
 }
 
 export const filterSource = {
-    bm: ['Element', 'OKX', 'Binance'],
-    qp: ['Blur', 'Opensea', 'Element', 'OKX', 'Binance'],
-    peace: ['Tesseract'],
-    ap: ['Blur', 'Opensea', 'Element', 'OKX', 'Binance'],
-    pf: ['Blur', 'Opensea', 'Element', 'OKX']
+  bm: ['Element', 'OKX', 'Binance'],
+  qp: ['Blur', 'Opensea', 'Element', 'OKX', 'Binance'],
+  peace: ['Tesseract'],
+  ap: ['Blur', 'Opensea', 'Element', 'OKX', 'Binance'],
+  pf: ['Blur', 'Opensea', 'Element', 'OKX'],
+  planet: ['Tesseract'],
 }
 
 export const platformIcon = {
@@ -38,41 +50,48 @@ export const queryName = {
   qp: 'Quartan Primes',
   bm: 'Bi-Mech',
   ap: 'Alpha Prestige',
-  pf: 'Pioneer of Fusionist'
+  pf: 'Pioneer of Fusionist',
+  planet: 'Fusionist Planet',
 }
 
 export const pageNames = {
-  bm: { name: 'Bi-Mech', link: "collections/Bi-mech.webp" },
-  qp: { name: 'Quartan Primes', link: "collections/Quartan Primes.webp" },
-  ap: { name: 'Alpha Prestige', link: "collections/Alpha Prestige.webp" },
-  peace: {name: 'PrimeACE', link: "collections/ace.svg"},
-  pf: { name: 'Pioneer of Fusionist', link: "collections/Pioneer of Fusionist.webp" }
+  bm: { name: 'Bi-Mech', link: 'collections/Bi-mech.webp' },
+  qp: { name: 'Quartan Primes', link: 'collections/Quartan Primes.webp' },
+  ap: { name: 'Alpha Prestige', link: 'collections/Alpha Prestige.webp' },
+  peace: { name: 'PrimeACE', link: 'collections/ace.svg' },
+  pf: { name: 'Pioneer of Fusionist', link: 'collections/Pioneer of Fusionist.webp' },
+  planet: { name: 'Fusionist Planet', link: 'collections/Fusionist Planet.png' }
 }
 
 export const bannerLinks = {
+  'Fusionist Planet': {
+    name: 'Fusionist Planet',
+    image: '/banner/planet.png',
+    link_to: '/planet'
+  },
   Bi_Mech: {
     name: 'Bi-Mech',
-    image: "/banner/bm.gif",
+    image: '/banner/bm.gif',
     link_to: '/bm'
   },
   Quartan_Primes: {
     name: 'Quartan Primes',
-    image: "/banner/qp.gif",
+    image: '/banner/qp.gif',
     link_to: '/qp'
   },
   Alpha_Prestige: {
     name: 'Alpha Prestige',
-    image: "/banner/ap.gif",
+    image: '/banner/ap.gif',
     link_to: '/ap'
   },
   PrimeACE: {
     name: 'PrimeACE',
-    image: "/banner/peace.png",
+    image: '/banner/peace.png',
     link_to: '/peace'
   },
   'Pioneer of Fusionist': {
     name: 'Pioneer of Fusionist',
-    image: "/banner/pioneer.png",
+    image: '/banner/pioneer.png',
     link_to: '/pf'
   }
 }
